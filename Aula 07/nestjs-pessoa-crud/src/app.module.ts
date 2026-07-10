289bf1b8500e3ff215f7aa04e01fcf611846e5f7
zzabbella <isa.camp.ferreira@gmail.com>
2026-07-09 21:48:42 -0400

    Adiciona projeto da Aula 07

---
+++ Aula 07/nestjs-pessoa-crud/src/app.module.ts
@@ -0,0 +1,16 @@
+﻿import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PessoaModule } from './pessoa/pessoa.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'database.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    PessoaModule,
+  ],
+})
+export class AppModule {}
---
+++ Aula 07/nestjs-pessoa-crud/src/pessoa/dto/create-user.dto.ts
@@ -0,0 +1,19 @@
+﻿import { IsString, IsEmail, IsNotEmpty, IsOptional, IsInt } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  nome: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  telefone?: string;
+
+  @IsOptional()
+  @IsInt()
+  idade?: number;
+}
